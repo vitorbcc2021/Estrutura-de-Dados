438761c1ff74a59f6ab945e5765ec982cab86d35
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-06-06 21:53:39 -0300

    Create Fila.java

---
+++ Aula 10/Exercicio 5/Fila.java
@@ -0,0 +1,29 @@
+package ed1dezcinco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fila {
+    List<Integer> fila = new ArrayList<>();
+    
+    public void push(int numero){
+        fila.add(numero);
+    }
+    
+    public int pop(){
+        int removido = fila.get(0);
+        fila.remove(0);
+        return removido;
+    }
+    
+    public boolean empty(){
+        return fila.isEmpty();
+    }
+    
+    public int tamanhoFila(){
+        return fila.size();
+    }
+    public boolean procurarNaFila(int valor){
+        return fila.contains(valor);
+    }
+}
