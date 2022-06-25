4f337ad10aaf18de4e637467c682748ededfe87a
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-06-25 16:52:24 -0300

    Add files via upload

---
+++ aula 13/Exercicio 3/CelulaPilha.java
@@ -0,0 +1,30 @@
+package ed1trezetres;
+
+public class CelulaPilha {
+    private Object elemento;
+    private CelulaPilha anterior;
+
+    public CelulaPilha(){}
+     
+    public CelulaPilha(Object elemento, CelulaPilha anterior) {
+        this.elemento = elemento;
+        this.anterior = anterior;
+    }
+
+    
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+
+    public CelulaPilha getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(CelulaPilha anterior) {
+        this.anterior = anterior;
+    }
+}
