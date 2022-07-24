a43b3fff06a8ba7527c10de175959ea3d85dd086
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-07-24 12:26:40 -0300

    Update and rename Aula 7/main.java to Aula 7/InvertePalavras/main.java

---
+++ Aula 7/InvertePalavras/main.java
@@ -0,0 +1,13 @@
+package editresum;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class EdiTresUm {
+    
+    public static void main(String[] args) {
+        Pilha p =  new Pilha();
+	p.invertePalavras("Macaco quer comer banana");
+        
+    }
+}
