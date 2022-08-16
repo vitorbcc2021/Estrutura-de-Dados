a7cebfe473be6756490db8ade2a22cc2e8c8d424
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-08-16 08:40:59 -0300

    Add files via upload

---
+++ ED1/Aula 11/Exercício 2/Celula.java
@@ -0,0 +1,32 @@
+package ed1onzedois;
+
+public class Celula {
+    private Celula proxima;
+    private Object elemento;
+
+    public Celula(Celula proxima, Object elemento) {
+        this.proxima = proxima;
+        this.elemento = elemento;
+    }
+    
+    public Celula(Object elemento){
+        this.elemento = elemento;
+    }
+
+    public Celula getProxima() {
+        return proxima;
+    }
+
+    public void setProxima(Celula proxima) {
+        this.proxima = proxima;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+    
+}
