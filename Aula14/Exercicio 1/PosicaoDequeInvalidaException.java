139ac0cf8fad2f7f34758861a1a0e13e9c9378d1
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-08-16 08:38:49 -0300

    Delete Aula14/Exercicio 1 directory

--- Aula14/Exercicio 1/PosicaoDequeInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package ed1quatorzeum;
-
-public class PosicaoDequeInvalidaException extends RuntimeException{
-
-    public PosicaoDequeInvalidaException(String message) {
-        super(message);
-    }
-    
-}
