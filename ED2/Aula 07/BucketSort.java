20e757974384c19edaf8bdf799bd1916b3ceb7f3
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-10-11 18:53:06 -0300

    BucketSort projeto final

--- ED2/Aula 07/BucketSort.java
+++ ED2/Aula 07/BucketSort.java
@@ -1,6 +1,6 @@
 package aula007;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Vector;
 
 import arquivo.Arquivo;
@@ -9,12 +9,18 @@ public class BucketSort {
     
     private long mov;
     private long comp;
+    private int maior;
+    private int menor;
+    private int[] vetor;
 
     public BucketSort(int[] vetor, String nomeArq) {
         this.mov = 0;
         this.comp = 0;
+        this.maior = Arrays.stream(vetor).max().orElse(Integer.MAX_VALUE);
+        this.menor = Arrays.stream(vetor).min().orElse(0);
+        this.vetor = vetor;
 
-        float[] vetorFloat = convertFloat(vetor);
+        float[] vetorFloat = convertFloat(vetor); // Nao contei como parte do algoritmo pois estou simulando uma situacao em que de fato um vetor de float seria passado, como o objetivo do exercicio e apenas calcular o tempo de execucao do algoritmo em si, eu tomei a liberdade de retirar algo que nao e do bucket sort pois atrapalharia uma analise real!
 
         long tempoIni = System.currentTimeMillis();
 
@@ -29,42 +35,87 @@ public class BucketSort {
     }
 
     private void bucketSort(float[] vetor){
+        //int tam = (this.maior - this.menor) / vetor.length;
+        int size = maior - menor + 1;
+
         @SuppressWarnings("unchecked")
-        Vector<Float>[] bucket = new Vector[vetor.length];
+        Vector<Float>[] bucketPositivos = new Vector[size];
+        @SuppressWarnings("unchecked")
+        Vector<Float>[] bucketNegativos = new Vector[size];
 
-        for (int i = 0; i < vetor.length; i++) {
-            bucket[i] = new Vector<Float>(); //cada posicao do meu vetor vai receber a instancia de um outro vetor
+        for (int i = 0; i < size; i++) {
+            bucketPositivos[i] = new Vector<Float>(); //cada posicao do meu balde vai receber a instancia de um outro vetor
+            bucketNegativos[i] = new Vector<Float>();
         }
 
         for (int i = 0; i < vetor.length; i++) {
-            float index = vetor[i] * vetor.length;
-            bucket[(int)index].add(vetor[i]);
+            if(vetor[i] >= 0){
+                float index = (vetor[i] - this.menor) / size;
+                bucketPositivos[(int) index].add(vetor[i]);
+            }
+            else{
+                float index = ((-vetor[i]) - this.menor) / size;
+                bucketNegativos[(int) index].add(vetor[i]); 
+            }
         }
 
-        for (Vector<Float> vector : bucket) {
-            Collections.sort(vector);
+        for (Vector<Float> vector : bucketPositivos) { // ordena cada um dos vetores do meu balde
+            insertionSort(vector);
+        }
+
+        for (Vector<Float> vector : bucketNegativos) {
+            insertionSort(vector);
         }
 
         int index = 0;
-        for (int i = 0; i < vetor.length; i++) {
-            for (int j = 0; j < bucket[i].size(); j++) {
-                vetor[index++] = bucket[i].get(j);
+        for (int i=size-1; i>=0; i--) {
+            for (int j = 0; j < bucketNegativos[i].size(); j++) {
+                this.vetor[index++] = bucketNegativos[i].get(j).intValue();
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < bucketPositivos[i].size(); j++) {
+                this.vetor[index++] = bucketPositivos[i].get(j).intValue();
             }
         }
 
     }
 
-    private float[] convertFloat(int[] vetor) {
-        int i = 0;
+    private Vector<Float> insertionSort(Vector<Float> vetor) {
+        Float chave;
+        
+        if(vetor.size() == 1) return vetor;
 
+        for(int i=1; i<vetor.size(); i++){
+            chave = vetor.get(i);
+            int j = i - 1;
+
+            this.comp++;
+            while(j>=0 && vetor.get(j) > chave){
+                this.comp++;
+                vetor.set(j+1, vetor.get(j));;
+                this.mov++;
+                j--;
+            }
+
+            vetor.set(j+1, chave);
+            mov++;
+        }
+
+        return vetor;
+    }
+
+    private float[] convertFloat(int[] vetor) {
         float[] aux = new float[vetor.length];
 
-        for (int num : vetor) {
-            aux[i] = (float) num;
-            i++;
+        for (int i = 0; i < aux.length; i++) {
+            aux[i] = (float) vetor[i];
         }
 
         return aux;
     }
+
+    
     
 }
